d8ceca9d1d69655084f1db670de613b51775bf00
kgao1997 <kgao1997@gmail.com>
2014-08-06 18:36:36 -0400

    added production environemnt

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,23 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	$env = $app->detectEnvironment(function() {
 
-));
+    # See if there's an environment.php file...
+    # If it exists, get environment string from there
+    if(file_exists(__DIR__.'/../environment.php')) {
+        return require __DIR__.'/../environment.php';
+    }
+    # If not found, assume production
+    else {
+        return 'production';
+    }
+
+});
+
+
+
 
 /*
 |--------------------------------------------------------------------------
