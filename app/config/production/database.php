cff92624e77bc896f56361a61e2cc72a1a05ad0c
kgao1997 <kgao1997@gmail.com>
2014-08-06 19:09:32 -0400

    added database.php

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'cardDatabase',
+            'username'  => 'emmaline',
+            'password'  => 'p5ORX4MS',
+        ),
+
+    ),
+
+);
